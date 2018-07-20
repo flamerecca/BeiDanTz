41b423db6d99bb8dc14931f322f5beb09f203394
recca <flamerecca711@gmail.com>
2018-07-21 00:51:15 +0800

    <feature>: 新增 TestServiceInterface

---
+++ app/Services/TestServiceInterface.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reccachao
+ * Date: 2018/7/21
+ * Time: 00:26
+ */
+
+namespace App\Services;
+
+use App\DTO\AnswerDTO;
+use App\DTO\QuestionDTO;
+
+interface TestServiceInterface
+{
+    /**
+     * 取得問題
+     * @return QuestionDTO
+     */
+    public function getQuestion():QuestionDTO;
+
+    /**
+     * 回傳答案狀況
+     * @param AnswerDTO $answerDTO
+     */
+    public function setAnswer(AnswerDTO $answerDTO):void;
+}
