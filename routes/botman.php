65d62fb08ce0bcb851a24a4a5a661908ac28c912
recca <flamerecca711@gmail.com>
2018-07-20 13:18:52 +0800

    <feature>: 測試中文

--- routes/botman.php
+++ routes/botman.php
@@ -6,4 +6,9 @@ $botman = resolve('botman');
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
+
+$botman->hears('單字', function ($bot) {
+    $bot->reply('開始背單字!');
+});
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
