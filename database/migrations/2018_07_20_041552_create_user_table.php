d5e0fc86a0751a755d93151ef07e7c006b7e7578
recca <flamerecca711@gmail.com>
2018-07-20 13:14:13 +0800

    <feature>: 加入telegram user table

---
+++ database/migrations/2018_07_20_041552_create_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telegram_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('telegram_id', 20)->comment('telegramID');
+            $table->integer('error')->comment('回答錯誤次數');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telegram_users');
+    }
+}
