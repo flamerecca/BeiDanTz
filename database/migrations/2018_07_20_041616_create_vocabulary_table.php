add6e77835207637af230e0d1b8189947a677bb6
recca <flamerecca711@gmail.com>
2018-07-20 13:14:47 +0800

    <feature>: 加入單字資料表

---
+++ database/migrations/2018_07_20_041616_create_vocabulary_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVocabularyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vocabularies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('vocabulary')->comment('單字');
+            $table->decimal('easiest_factor')->comment('單字簡易程度');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vocabularies');
+    }
+}
