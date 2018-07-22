1cbb995d5fa5920031cb5ab5c69aeaa48d00e82e
recca <flamerecca711@gmail.com>
2018-07-22 12:30:35 +0800

    <feature>: 加上easiness factor 以及對應測試

---
+++ app/Services/EasinessFactorService.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reccachao
+ * Date: 2018/7/21
+ * Time: 01:15
+ */
+
+namespace App\Services;
+
+/**
+ * Class EasinessFactorService
+ * @package App\Services
+ */
+class EasinessFactorService
+{
+    /**
+     * 計算新的 Easiness Factor
+     * @param string $easinessFactor
+     * @param int $answerStatus
+     * @return string
+     */
+    public function getNewEasinessFactor(string $easinessFactor, int $answerStatus):string
+    {
+        if (!is_numeric($easinessFactor)) {
+            throw new \InvalidArgumentException('easiness factor is not numeric');
+        }
+
+        if (bccomp($easinessFactor, '999999999') === 1) {
+            throw new \InvalidArgumentException('easiness factor is too big');
+        }
+
+        $change = (string)(0.1 - (5 - $answerStatus) * (0.08 + (5 - $answerStatus) * 0.02));
+        $newEasinessFactor = bcadd($easinessFactor, $change, 3);
+
+        // 如果新數值小於 1.3，則回傳 1.3
+        if (bccomp($newEasinessFactor, '1.3') === -1) {
+            return '1.300';
+        }
+        return $newEasinessFactor;
+    }
+}
---
+++ tests/Unit/EasinessFactorServiceTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reccachao
+ * Date: 2018/7/21
+ * Time: 01:37
+ */
+
+namespace Tests\Unit;
+
+use App\Services\EasinessFactorService;
+use Tests\TestCase;
+
+class EasinessFactorServiceTest extends TestCase
+{
+    private $easinessFactorService;
+    /**
+     * EasinessFactorServiceTest constructor.
+     * @param null|string $name
+     * @param array $data
+     * @param string $dataName
+     */
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        $this->easinessFactorService = new EasinessFactorService();
+        parent::__construct($name, $data, $dataName);
+    }
+
+    public function testNewEasinessFactorIsCorrect(): void
+    {
+        $newEasinessFactor = $this->easinessFactorService->getNewEasinessFactor('2.500', 0);
+        $this->assertEquals('1.700', $newEasinessFactor);
+    }
+
+    public function testMinimumNewEasinessFactorIsOnePointThree()
+    {
+        $newEasinessFactor = $this->easinessFactorService->getNewEasinessFactor('0', 0);
+        $this->assertEquals('1.300', $newEasinessFactor);
+    }
+
+    public function testIfOldEasinessFactorIsNotStringShouldThrowException()
+    {
+        $this->expectException(\TypeError::class);
+        $this->easinessFactorService->getNewEasinessFactor( [], 0);
+    }
+
+    public function testIfOldEasinessFactorIsNotNumericShouldThrowException()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('easiness factor is not numeric');
+
+        $this->easinessFactorService->getNewEasinessFactor('aaa', 0);
+    }
+
+    public function testIfAnswerStatusIsNotNumberShouldThrowException()
+    {
+        $this->expectException(\TypeError::class);
+        $this->easinessFactorService->getNewEasinessFactor('0', 'aa');
+    }
+
+    public function testIfOldEasinessFactorIsTooBig()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('easiness factor is too big');
+
+        $this->easinessFactorService->getNewEasinessFactor(
+            '99999999999999999999999999',
+            0
+        );
+    }
+}
