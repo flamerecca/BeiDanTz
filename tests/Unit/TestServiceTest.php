e35248b2d61f5c00ad5686533b747f153144b519
recca <flamerecca711@gmail.com>
2018-08-03 23:22:48 +0800

    <test>: add different senario tests
    check if there is vocabularies needed to be reviewed,
    getQuestion() can get those vocabulary correctly.

--- tests/Unit/TestServiceTest.php
+++ tests/Unit/TestServiceTest.php
@@ -107,6 +107,36 @@ class TestServiceTest extends TestCase
         $this->assertEquals(4, count($question->getOptions()));
     }
 
+    public function testIfUserHasTodayVocabularyGetQuestionShouldWorkProperly()
+    {
+        $vocabularyId = 3;
+        $this->telegramUser->vocabularies()->sync([
+            $vocabularyId => [
+                'review_date' =>  (new \DateTime('today'))->format('Y-m-d'),
+                'easiest_factor' => 3.000,
+                'continuing_correct_times' => 5,
+            ]
+        ]);
+        $question = $this->testService->getQuestion($this->telegramUser);
+        $this->assertInstanceOf(QuestionDTO::class, $question);
+        $this->assertEquals($question->getVocabularyId(), $vocabularyId);
+    }
+
+    public function testIfUserHasPreviousVocabularyGetQuestionShouldWorkProperly()
+    {
+        $vocabularyId = 3;
+        $this->telegramUser->vocabularies()->sync([
+            $vocabularyId => [
+                'review_date' =>  '1987-01-01',
+                'easiest_factor' => 3.000,
+                'continuing_correct_times' => 5,
+            ]
+        ]);
+        $question = $this->testService->getQuestion($this->telegramUser);
+        $this->assertInstanceOf(QuestionDTO::class, $question);
+        $this->assertEquals($question->getVocabularyId(), $vocabularyId);
+    }
+
     public function testAnswerFirstTimePass()
     {
         $vocabularyId = 3;
